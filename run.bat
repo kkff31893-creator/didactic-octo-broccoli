e14e2e376d456fb8308aa939e9f386c62f54ca88
kkff31893-creator <kkff31893@gmail.com>
2026-08-27 23:52:01 +0500

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 > nul
-title Telegram Garant Bot
-cd /d "%~dp0"
-echo ==============================================
-echo  Запуск Telegram Garant Bot...
-echo ==============================================
-python main.py
-pause
